78fcbe8d681a83637e7415ee638467ce9b88299c
gxk <1005035266@qq.com>
2020-07-23 17:25:39 +0800

    fix rune to string

--- model/bot_model.go
+++ model/bot_model.go
@@ -12,12 +12,12 @@ type Bot struct{
 
 type BotDto struct{
 	Id string `json:"id" gorm:"type:varchar(50);primary_key;unique_index:id_UNIQUE;not null"`
-	Distribution rune `json:"distribution" gorm:"type:enum('0','1','2','3');primary_key;not null"`
+	Distribution string`json:"distribution" gorm:"type:enum('0','1','2','3');primary_key;not null"`
 }
 
 const (
-	PersperAlgorithm = '0'
-	Commits = '1'
-	ChangedLines = '2'
-	IdenticalAmount = '3'
+	PersperAlgorithm = "0" //persper算法
+	Commits = "1" //commit数量
+	ChangedLines = "2" //代码行数
+	IdenticalAmount = "3"
 )
